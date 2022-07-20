4469873d3f1f7b4fc8ad616066ba88b027d0b75d
Xavster838 <francisguitart@gmail.com>
2022-07-20 11:20:10 -0700

    moved rename_fasta logic to python script so that I can use a conda environment in the rule.

---
+++ workflow/scripts/rename_fasta.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Author: Xavi Guitart
+#   change names in fasta. output new named fasta and a table describing the contig/read name changes.
+name_change_df = pd.DataFrame(columns = ['original_fasta', 'original_name', 'new_fasta' , 'new_name'])
+with open(snakemake.input.samp_fasta) as original_file:
+    records = [ i for i in SeqIO.parse(original_file, 'fasta') ] #generate list of fasta sequences
+    for i,seq in enumerate(records):
+        name_change_df.loc[i] = [ snakemake.input.samp_fasta , seq.id, output.new_fa ,f"{snakemake.wildcards.sm}__{snakemake.wildcards.h}__{i}"]
+        seq.id = name_change_df.loc[i, "new_name"]
+        seq.description = "" #description added to end of header. was old name previously.
+    with open(snakemake.output.new_fa, "w") as output_handle:
+        SeqIO.write(records, output_handle, "fasta")
+name_change_df.to_csv( snakemake.output.old_new_name_map , sep = "\t", header = True, index = False)
